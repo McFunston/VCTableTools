6b5fb8ab9d34ecb4324d217a2faa496b246b88b0
Mica <mica.funston@gmail.com>
2022-11-09 14:33:46 -0500

    Add FindResponse and ITableFinder

---
+++ VisualCronTableTools/FindResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisualCronTableTools
+{
+	public class FindResponse
+	{
+		public FindResponse(bool success, string message, Dictionary<string, string> findings)
+		{
+			Success = success;
+			Message = message;
+			Findings = findings;
+		}
+		public bool Success { get; set; }
+		public string Message { get; set; }
+		public Dictionary<string, string> Findings { get; set; } // Key is location (A2, G41, etc) Value is cell value
+	}
+}
+
---
+++ VisualCronTableTools/Tools/ITableFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace VisualCronTableTools.Tools
+{
+	public interface ITableFinder
+	{
+        public delegate bool Matcher(string searchTerm, string itemToSearch);
+
+
+    }
+}
+
