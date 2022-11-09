ccc9edd3833e7d036d0759c0dca75a2403046b90
Mica <mica.funston@gmail.com>
2022-11-09 13:25:19 -0500

    Add XLSXTable class

--- VisualCronTableTools/Tools/XLSX/XLSXTable.cs
+++ VisualCronTableTools/Tools/XLSX/XLSXTable.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace VisualCronTableTools.Tools.XLSX
 {
 	public class XLSXTable
 	{
-		public XLSXTable()
+        /* A Microsoft Office Open XML format table */
+        IXLWorksheet Worksheet;
+
+        public XLSXTable(string path, string sheetName, bool loadHeaders = true)
+        {
+            Worksheet = GetWorksheet(path, sheetName, loadHeaders);
+        }
+
+		private IXLWorksheet GetWorksheet(string path, string sheetName, bool loadHeaders = true)
 		{
-		}
-	}
+            var workbook = new XLWorkbook(path);
+            IXLWorksheet sheet = workbook.Worksheet(sheetName);
+            return sheet;
+        }
+    }
 }
 
