5825676f778f4dc1ac9be98e2181b53a62a2d607
Mica <mica.funston@gmail.com>
2022-11-09 15:00:47 -0500

    Change Findings to a List of Dictionaries

--- VisualCronTableTools/FindResponse.cs
+++ VisualCronTableTools/FindResponse.cs
@@ -5,7 +5,12 @@ namespace VisualCronTableTools
 {
 	public class FindResponse
 	{
-		public FindResponse(bool success, string message, Dictionary<string, string> findings)
+        /* A FindResponse will consist of the following properties
+			Success: True if found, False if not
+			Message: A string describing the findings ("Two items found")
+			Findings: A list of dictionaries with the contents of the rows where the searched value was found ("A9": "Boston", "B9": "Massachusetts" for example) */
+
+		public FindResponse(bool success, string message, List<Dictionary<string, string>> findings)
 		{
 			Success = success;
 			Message = message;
@@ -13,7 +18,7 @@ namespace VisualCronTableTools
 		}
 		public bool Success { get; set; }
 		public string Message { get; set; }
-		public Dictionary<string, string> Findings { get; set; } // Key is location (A2, G41, etc) Value is cell value
+		public List<Dictionary<string, string>> Findings { get; set; } // Key is location (A2, G41, etc) Value is cell value 
 	}
 }
 
--- VisualCronTableTools/Tools/ITableFinder.cs
+++ VisualCronTableTools/Tools/ITableFinder.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using ClosedXML.Excel;
+using System.Collections.Generic;
+
 namespace VisualCronTableTools.Tools
 {
 	public interface ITableFinder
 	{
         public delegate bool Matcher(string searchTerm, string itemToSearch);
 
-
+        public FindResponse FindAll(string searchTerm, string column, Matcher matcher); //Find all rows where the cell in "column" matches searchTerm
+        public FindResponse FindFirst(string searchTerm, string column, Matcher matcher); //Same as FindAll but returns the first row found
+        public FindResponse FindAllWithout(string searchTerm, string column, string killColumn, Matcher matcher); //Find
     }
 }
 
