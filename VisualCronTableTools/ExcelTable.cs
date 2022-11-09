64230357c428d3679a50ac4ca096aa7356e1ad56
Mica <mica.funston@gmail.com>
2022-11-09 12:38:08 -0500

    Initial. Create some folder structure.

---
+++ VisualCronTableTools/ExcelTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+
+namespace VisualCronTableTools
+{
+	public class ExcelTable
+	{
+		public ExcelTable()
+		{
+		}
+	}
+}
+
---
+++ VisualCronTableTools/Tools/XLSX/XLSXTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+
+namespace VisualCronTableTools.Tools.XLSX
+{
+	public class XLSXTable
+	{
+		public XLSXTable()
+		{
+		}
+	}
+}
+
