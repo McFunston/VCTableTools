129b51339c600f295ad4bad3fd13ef6565c260a3
Mica <mica.funston@gmail.com>
2022-11-15 14:01:53 -0500

    Create TableSearcher static class

--- VisualCronTableTools/TableSearcher.cs
+++ VisualCronTableTools/TableSearcher.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using VisualCronTableTools.Models;
+using VisualCronTableTools.Tools;
 using VisualCronTableTools.Tools.XLS;
 
 namespace VisualCronTableTools
 {
 	public static class TableSearcher
 	{
-		public FindResponse FindAllExact(string path, string sheetName, string searchColumn, string searchTerm)
+		public static FindResponse FindAllExact(string path, string sheetName, string searchColumn, string searchTerm)
 		{
 			XLSTable table = new XLSTable(path, sheetName);
 			TableListDictionary tableListDictionary = table.tableListDictionary;
 
+			FindResponse findResponse = tableListDictionary.FindAll(searchTerm, searchColumn, Matcher.findEquals);
+			return findResponse;
 
 		}
 	}
