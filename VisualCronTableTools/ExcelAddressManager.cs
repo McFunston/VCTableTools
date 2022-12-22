a154e974a253a9fbf6ddf21ea6e85e77b63e4b1c
Mica <mica.funston@gmail.com>
2022-12-22 14:12:36 -0500

    Add ExcelAddressManager.GetRow and .GetColumn and test for each.

---
+++ VisualCronTableTools/ExcelAddressManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace VisualCronTableTools
+{
+	public static class ExcelAddressManager
+	{
+		public static string GetRow(string address)
+		{
+			string row = "";
+
+			foreach (var c in address)
+			{
+				if (char.IsNumber(c))
+				{
+					row += c;
+				}
+			}
+
+			return row;
+		}
+
+		public static string GetColumn(string address)
+		{
+			string column = "";
+
+			foreach (var c in address)
+			{
+				if (!(char.IsNumber(c)))
+				{
+					column += c;
+				}
+			}
+			return column;
+		}
+	}
+}
+
---
+++ VisualCronTableToolsTests/ExcelAddressManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using VisualCronTableTools;
+
+namespace VisualCronTableToolsTests
+{
+    [TestClass]
+    public class ExcelAddressManagerTests
+	{
+		[TestMethod]
+		public void TestGetRow()
+		{
+			//Arrange
+			string address = "CA155";
+			string expected = "155";
+
+			//Act
+			string actual = ExcelAddressManager.GetRow(address);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestGetColumn()
+        {
+            //Arrange
+            string address = "CA155";
+            string expected = "CA";
+
+            //Act
+            string actual = ExcelAddressManager.GetColumn(address);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
+
